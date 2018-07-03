5c4a4cfbc4e90e4d262120958e8836bfd9e92398
RavitejaLam <ravitejalam28@gmail.com>
2018-07-03 16:12:15 +0530

    PROJECTROOT

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(PROJECT_PATH)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_PATH=os.path.join(PROJECT_PATH,'static')
 STATICFILES_DIRS = (
