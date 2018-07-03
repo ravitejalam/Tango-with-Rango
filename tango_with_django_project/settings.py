576b7865977227aaf83fca1c97981481f9c2a5a6
RavitejaLam <ravitejalam28@gmail.com>
2018-07-03 13:13:06 +0530

    Collectstatic

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(PROJECT_PATH)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_PATH=os.path.join(PROJECT_PATH,'static')
 STATICFILES_DIRS = (
     STATIC_PATH,
