1bb961b758adc49b08cae23dbc4f5ece820376b4
RavitejaLam <ravitejalam28@gmail.com>
2018-07-05 13:36:04 +0530

    registration working

--- rango/views.py
+++ rango/views.py
@@ -115,7 +115,7 @@ def add_page(request, category_name_slug):
 def register(request):
     registered = False
     if request.method == 'POST':
-        user_form = UserForm(data=reuserquest.POST)
+        user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
